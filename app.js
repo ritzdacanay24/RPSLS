13f693fd7b99c8a8353ae79ec38b1eb679ab5466
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-06-23 18:17:19 -0700

    Defined variables within the constructor

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
 class game {
     constructor(){
-
+        this.score = 0;
+        this.gestureOptions = [];
     }
 }
 
 class player {
-    constructor(){
-        
+    constructor(name){
+        this.points = 0;
+        this.name = name;
     }
 }
 
